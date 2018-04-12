23220070a5628a69f960bf37cf14e007493466e7
reynin14 <kyonzon14@gmail.com>
2018-04-11 18:11:45 -0700

    set up autoscroll feature using react-scroll-to-component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import scrollToComponent from 'react-scroll-to-component';
 
 import { Container } from './components/container';
 
@@ -8,11 +9,27 @@ import { Portfolio } from './scenes/Portfolio';
 import { Contact } from './scenes/Contact';
 
 class App extends Component {
+
+  componentDidMount() {
+    scrollToComponent(this.About, { offset: 0, align: 'middle', duration: 500, ease:'inCirc'});
+  }
+
+  scrollToAbout = () => {
+    console.log('scrolling to about');
+    scrollToComponent(this.About, { offset: 0, align: 'top', duration: 1500})
+  }
+
   render() {
     return (
       <Container>
-        <Home />
-        <About />
+        <section>
+          <Home scrollToAbout={this.scrollToAbout} />
+        </section>
+
+        <section ref={(section) => { this.About = section; }}>
+          <About />
+        </section>
+
         <Portfolio />
         <Contact />
       </Container>
--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-import { Navbar } from '../../components/navbar'
-
-const Home = () => {
+const Home = ({scrollToAbout}) => {
   return(
     <div style={styles.container}>
       <div style={styles.text}>
         Hello, I'm Kevin Yonzon.<br></br>
         I'm a full-stack web developer.
       </div>
+      <button onClick={scrollToAbout}>View My Work</button>
     </div>
   )
 };
+
+Home.propTypes = {
+  scrollToAbout: PropTypes.func,
+}
 
 export default Home;
